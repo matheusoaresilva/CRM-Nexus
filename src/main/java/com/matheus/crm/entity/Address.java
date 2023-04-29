d69875276ea95551e0be92d14ddbba9b58ef3c0f
Matheus Soares <matheusmatias2622@gmail.com>
2023-04-29 17:33:15 -0300

    feat: added address entity

---
+++ src/main/java/com/matheus/crm/entity/Address.java
@@ -0,0 +1,113 @@
+package com.matheus.crm.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "address")
+@Entity(name = "address")
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column
+	private String street;
+	@Column
+	private Integer number;
+	@Column
+	private String neighborhood;
+	@Column
+	private String city;
+	@Column
+	private String state;
+	@Column
+	private String country;
+	@Column
+	private String zipcode;
+//    private Customer customer;
+
+	public Address() {
+	}
+
+	public Address(Long id, String street, Integer number, String neighborhood, String city, String state,
+			String country, String zipcode) {
+
+		this.id = id;
+		this.street = street;
+		this.number = number;
+		this.neighborhood = neighborhood;
+		this.city = city;
+		this.state = state;
+		this.country = country;
+		this.zipcode = zipcode;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+}
