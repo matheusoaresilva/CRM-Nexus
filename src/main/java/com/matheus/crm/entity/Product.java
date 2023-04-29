3af330cac85cea74eb053a025c961728985f9bb2
Matheus Soares <matheusmatias2622@gmail.com>
2023-04-29 17:19:18 -0300

    fix: application.properties file setings

---
+++ src/main/java/com/matheus/crm/entity/Product.java
@@ -0,0 +1,61 @@
+package com.matheus.crm.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "product")
+@Entity(name = "product")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+//    private Category category;
+    private Long sku;
+//    private Supplier supplier;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	public Long getSku() {
+		return sku;
+	}
+	public void setSku(Long sku) {
+		this.sku = sku;
+	}
+    
+    
+}
