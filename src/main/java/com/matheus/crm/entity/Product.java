8d9c18aae83a410762cdb1b15cfb13c9e8466623
Matheus Soares <matheusmatias2622@gmail.com>
2023-04-29 17:23:31 -0300

    feat: added product entity

--- src/main/java/com/matheus/crm/entity/Product.java
+++ src/main/java/com/matheus/crm/entity/Product.java
@@ -1,5 +1,6 @@
 package com.matheus.crm.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,53 +10,82 @@ import javax.persistence.Table;
 @Table(name = "product")
 @Entity(name = "product")
 public class Product {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String description;
-    private Double price;
-    private String imgUrl;
+	private Long id;
+	@Column
+	private String name;
+	@Column
+	private String description;
+	@Column
+	private Double price;
+	@Column
+	private String imgUrl;
 //    private Category category;
-    private Long sku;
+	@Column
+	private Long sku;
 //    private Supplier supplier;
+
+	public Product() {
+
+	}
+
+	public Product(Long id, String name, String description, Double price, String imgUrl, Long sku) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+		this.sku = sku;
+	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public Double getPrice() {
 		return price;
 	}
+
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+
 	public String getImgUrl() {
 		return imgUrl;
 	}
+
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
+
 	public Long getSku() {
 		return sku;
 	}
+
 	public void setSku(Long sku) {
 		this.sku = sku;
 	}
-    
-    
+
 }
