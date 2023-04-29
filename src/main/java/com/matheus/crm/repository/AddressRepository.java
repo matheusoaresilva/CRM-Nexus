ce1a12713613c92b7a5fce8eb050501913e9a37d
Matheus Soares <matheusmatias2622@gmail.com>
2023-04-29 18:48:05 -0300

    feat: added Address repository

---
+++ src/main/java/com/matheus/crm/repository/AddressRepository.java
@@ -0,0 +1,11 @@
+package com.matheus.crm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheus.crm.entity.Address;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long>{
+
+}
