2c3f4af4872882616e490597d73b9b8969937c47
Matheus Soares <matheusmatias2622@gmail.com>
2023-04-29 18:49:46 -0300

    feat: added Category entity

---
+++ src/main/java/com/matheus/crm/entity/Category.java
@@ -0,0 +1,56 @@
+package com.matheus.crm.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "category")
+@Entity(name = "category")
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column
+	private String name;
+	@Column
+	private String description;
+
+	public Category() {
+	}
+
+	public Category(Long id, String name, String description) {
+
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
