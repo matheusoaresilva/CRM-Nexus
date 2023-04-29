e7df7f27db334e87429a0c535cbb46f0c03d42c6
Matheus Soares <matheusmatias2622@gmail.com>
2023-04-29 19:59:05 -0300

    fix: correcting import

--- src/main/java/com/matheus/crm/repository/CategoryRepository.java
+++ src/main/java/com/matheus/crm/repository/CategoryRepository.java
@@ -1,11 +1,14 @@
 package com.matheus.crm.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.matheus.crm.entity.Address;
+import com.matheus.crm.entity.Category;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Address, Long>{
+public interface CategoryRepository extends JpaRepository<Category, Long>{
 
+	Optional<Category> findById(Long id);
 }
