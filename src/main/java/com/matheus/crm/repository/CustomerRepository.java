44de1b435eee4d947cb5a937dc83a03733af4579
Matheus Soares <matheusmatias2622@gmail.com>
2023-04-29 23:21:00 -0300

    feat: Added method findById

--- src/main/java/com/matheus/crm/repository/CustomerRepository.java
+++ src/main/java/com/matheus/crm/repository/CustomerRepository.java
@@ -1,5 +1,7 @@
 package com.matheus.crm.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.matheus.crm.entity.Customer;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
 
+	Optional<Customer> findById(Long id);
 }
--- src/main/java/com/matheus/crm/repository/SupplierRepository.java
+++ src/main/java/com/matheus/crm/repository/SupplierRepository.java
@@ -1,5 +1,7 @@
 package com.matheus.crm.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.matheus.crm.entity.Supplier;
 
 @Repository
 public interface SupplierRepository extends JpaRepository<Supplier, Long>{
-
+	Optional<Supplier> findById(Long id);
 }
