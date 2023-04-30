024606b34acb46dcd36f31176527be9718ae8df2
Matheus Soares <matheusmatias2622@gmail.com>
2023-04-30 00:11:15 -0300

    perf: changing exception class name for better code reuse

---
+++ src/main/java/com/matheus/crm/exception/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.matheus.crm.exception;
+
+public class NotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public NotFoundException(String message) {
+		super(message);
+	}
+}
