7e31291ad302227f52baa3519e4f0db613a7f9c5
Matheus Soares <matheusmatias2622@gmail.com>
2023-06-08 17:55:48 -0300

    feat: Created ShippingCompany

---
+++ src/main/java/com/matheus/crm/entity/ShippingCompany.java
@@ -0,0 +1,82 @@
+package com.matheus.crm.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "shippingcompany")
+@Entity(name = "shippingcompany")
+public class ShippingCompany implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column
+    private String name;
+	@Column
+    private String cnpj;
+	@Column
+    private String phone;
+	@Column
+    private String email;
+//    private ArrayList<Address> addresses = new ArrayList<>();
+//    private ArrayList<Order> orders = new ArrayList<>();
+    
+    
+    
+    
+    public ShippingCompany(Long id, String name, String cnpj, String phone, String email) {
+		this.id = id;
+		this.name = name;
+		this.cnpj = cnpj;
+		this.phone = phone;
+		this.email = email;
+	}
+    
+    
+	public ShippingCompany() {
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+    
+    
+
+}
